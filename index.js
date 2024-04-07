674862b077042e0af7d80d812b6abc4ca0bfe63c
Siebe Geyskens <siebe-geyskens@hotmail.com>
2024-04-07 22:07:41 +0100

    clean up js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // Immediately invoked function expression to not pollute the global scope.
 (function () {
-  let degrees = 0;
-  let degreesMin = 500;
-  let degreesMax = 3000;
-  let timeMin = 2;
-  let timeMax = 6;
-  let zoneSize = 18; // deg
-  let serviceZones = {
+  // wheel spinning parameters
+  const options = {
+    degreesMin: 500, // degrees
+    degreesMax: 3000, // degrees
+    timeMin: 2, // seconds
+    timeMax: 6, // seconds
+  };
+
+  let degrees;
+  let degreesNormalized;
+  const zoneSize = 18; // deg
+  const serviceZones = {
     1: "Medijski zakup",
     2: "Spletne strani in aplikacije",
     3: "Spletno oglaševanje",
@@ -32,40 +37,50 @@
   const wheel = document.getElementById("wheel");
   const button = document.getElementById("button");
 
-  function handleWin(degrees) {
-    console.log(degrees);
-    const winningService = Math.ceil(degrees / zoneSize);
-    alert(serviceZones[winningService]);
+  // Calculate how long the wheels spins based on how far it spins
+  function calculateSpinningTime(degrees) {
+    //https://www.30secondsofcode.org/js/s/clamp-or-map-number-to-range/
+    return (
+      ((degrees - options.degreesMin) * (options.timeMax - options.timeMin)) /
+        (options.degreesMax - options.degreesMin) +
+      options.timeMin
+    );
   }
 
-  button.addEventListener("click", () => {
+  function spinWheel() {
     // Disable the button while spinning
     button.style.pointerEvents = "none";
     // Calculate a new rotation between degreesMin and degreesMax
-    degrees = Math.floor(degreesMin + Math.random() * (degreesMax - degreesMin));
-    // Calculate how long the wheels spins based on how far it spins
-    //https://www.30secondsofcode.org/js/s/clamp-or-map-number-to-range/
-    const mapNumRange = (num, inMin, inMax, outMin, outMax) =>
-      ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
-    spinningTime = mapNumRange(degrees, degreesMin, degreesMax, timeMin, timeMax);
+    degrees = Math.floor(
+      options.degreesMin + Math.random() * (options.degreesMax - options.degreesMin)
+    );
+    degreesNormalized = degrees % 360;
+    spinningTime = calculateSpinningTime(degrees);
     // Set the transition on the wheel
     wheel.style.transition = `all ${spinningTime}s ease-out`;
-    // button.style.transition = `all ${spinningTime}s ease-out`;
     // Rotate the wheel
     wheel.style.transform = `rotate(${degrees}deg)`;
-    // button.style.transform = `rotate(${degrees}deg)`;
-  });
+  }
 
-  wheel.addEventListener("transitionend", () => {
+  function resetWheel(degrees) {
     // Enable button events when spin is over
     button.style.pointerEvents = "auto";
     // Set to the weel to a "natural" 360 degree value
     wheel.style.transition = "none";
-    // button.style.transition = "none";
-    const degreesNormalized = degrees % 360;
     wheel.style.transform = `rotate(${degreesNormalized}deg)`;
-    // button.style.transform = `rotate(${degreesNormalized}deg)`;
-    // Calculate the winning service
-    handleWin(degreesNormalized);
+  }
+
+  function handleWin(degrees) {
+    // RENDER WINNING SERVICE HERE
+    const serviceZone = Math.ceil(degreesNormalized / zoneSize);
+    const winningService = serviceZones[serviceZone];
+    alert(winningService);
+  }
+
+  button.addEventListener("click", spinWheel);
+
+  wheel.addEventListener("transitionend", () => {
+    resetWheel(degrees);
+    handleWin(degrees);
   });
 })();
